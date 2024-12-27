9c8b18e22a13743ac4ece4158c813d82a8fa3ec2
Christian Koch Echeverria <ko.che@web.de>
2024-12-27 19:24:26 +0100

    Initial Commit

---
+++ CheckList/CheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListApp.swift
+//  CheckList
+//
+//  Created by Christian Koch Echeverria on 27.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
